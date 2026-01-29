f933e558855a33055235df40df935889834d5d58
Vortex7x <mostafa1and1mahmoud1@gmail.com>
2026-01-29 08:01:42 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import sys
+from ui import show_banner, hacker_intro, show_menu
+from engine import analyze_ip
+from io_handler import read_ips_from_file
+
+def main():
+    show_banner()
+    hacker_intro("VORTEX-IP", 0.05)
+
+    args = sys.argv[1:]
+    if args:
+        for ip in args:
+            analyze_ip(ip)
+        return
+
+    while True:
+        choice = show_menu()
+        if choice == "1":
+            ip = input("IP: ").strip()
+            analyze_ip(ip)
+        elif choice == "2":
+            ips = input("IPs (space separated): ").split()
+            for ip in ips:
+                analyze_ip(ip)
+        elif choice == "3":
+            path = input("File path: ").strip()
+            for ip in read_ips_from_file(path):
+                analyze_ip(ip)
+        elif choice == "0":
+            print("Bye 👋")
+            break
+        else:
+            print("Invalid choice!")
+
+if __name__ == "__main__":
+    main()
